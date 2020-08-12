7f69394531c05de780174eb280922df2642d1c22
fransiska <fransiska.basoeki@gmail.com>
2020-08-12 16:46:00 +0100

    Use recursive to get the album folder

--- play_music_by_folder.py
+++ play_music_by_folder.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import os
+from subprocess import Popen, PIPE
+import logging
+import time
 import random
 import epapermpd.mpd_functions as mpd
-import logging
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -11,24 +13,41 @@ mpd_music_directory = "/share/music"
 mpd_top_directory = "fransiska"
 base_directory = mpd_music_directory + "/" + mpd_top_directory
 
-def get_random_directory(directory):
-    return random.choice([x for x in os.listdir(directory)])
+def get_album(directory):
+    files = [x for x in os.listdir(directory)]
+    music_files = [x for x in files if any([os.path.splitext(x)[1] == ext for ext in [".mp3",".flac",".ape",".m4a"]])]
+    if len(music_files):
+        return directory
+    else:
+        folders = [x for x in files if os.path.isdir(os.path.join(directory, x))]
+        if len(folders):
+            # Go deeper
+            random_folder = random.choice(folders)
+            return get_album(os.path.join(directory, random_folder))
+        else:
+            # Start again
+            return get_album(base_directory)
 
 def load_album():
-    artist = get_random_directory(base_directory)
-    album = get_random_directory(base_directory + "/" + artist)
-    album_path = mpd_top_directory + "/" + artist + "/" + album
-    logging.debug("Loading {} : {}".format(artist, album))
-    cmd = "mpc clear; mpc ls '{}' | mpc add ; mpc play".format(album_path)
-    os.system(cmd)
+    album_path = get_album(base_directory)[len(mpd_music_directory)+1:]
+    os.system("mpc clear")
+    p1 = Popen(["mpc", "ls", album_path], stdout=PIPE, stderr=PIPE)
+    p2 = Popen(["mpc", "add"], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
+    out,err = p2.communicate()
+    if len(p1.stderr.read()):
+        raise Exception(err)
+    os.system("mpc play")
 
 def main():
+    os.system("mpc stop")
     while True:
-        logging.debug("Loading new album")
-        try: load_album()
-        except Exception as e: logging.error("Error in loading album: {}".format(e))
-        mpd.wait_until_playing()
-        mpd.wait_until_stopped()
+        try:
+            load_album()
+            mpd.wait_until_playing()
+            mpd.wait_until_stopped()
+        except Exception as e:
+            logging.error("Error in loading album: {}".format(e))
+            time.sleep(1)
 
 if __name__ == "__main__":
     main()
